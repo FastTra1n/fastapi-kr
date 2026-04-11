13e5eb3d2ac7a308725b6c79564622ac0e76f327
critical_error <->
2026-04-11 14:32:41 +0300

    feat(kr3): solved 6.5 problem.

--- kr3/tasks/task5/app.py
+++ kr3/tasks/task5/app.py
@@ -1,11 +1,17 @@
 import datetime
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from pyrate_limiter import Duration, Limiter, Rate
+from fastapi_limiter.depends import RateLimiter
 import jwt
+from passlib.context import CryptContext
 
-from db import get_user_from_db
+from db import get_user_from_db, add_user_to_db
+from models import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = 'secret_key'
 ALGORITHM = 'HS256'
@@ -34,17 +40,45 @@ def get_user_from_token(token: str = Depends(oauth2_scheme)):
             detail="Invalid access token."
         )
 
-@app.post("/login")
+@app.post(
+    "/register",
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(RateLimiter(limiter=Limiter(Rate(1, Duration.MINUTE))))]
+)
+async def register(credentials: User):
+    if get_user_from_db(credentials.username) is not None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User already exists."
+        )
+    hashed_password = pwd_context.hash(credentials.password)
+    new_user = User(username=credentials.username, password=hashed_password)
+    add_user_to_db(new_user)
+
+    return {"message": "New user created!"}
+
+
+
+@app.post(
+    "/login",
+    dependencies=[Depends(RateLimiter(limiter=Limiter(Rate(5, Duration.MINUTE))))]
+)
 async def login(credentials: OAuth2PasswordRequestForm = Depends()):
     user = get_user_from_db(credentials.username)
-    if user and credentials.password == user.password:
-        token = create_jwt_token({"sub": user.username})
-        return {"access_token": token}
-    
-    raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials."
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
         )
+    
+    if not pwd_context.verify(credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authorization failed."
+        )
+    
+    token = create_jwt_token({"sub": user.username})
+    return {"access_token": token}
 
 @app.get("/protected_resource")
 async def protected_resource(user: str = Depends(get_user_from_token)):
